cb2bde5d164ccf7121fe12c0f3292d7d1fd9d6bc
R2D2 <aleksandrswork61@gmail.com>
2023-03-31 22:28:00 +0300

    Added lessons to new repo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Introduction,
+  PanGestureBasics,
+  InterpolatedScrollview,
+  FloatingHearts,
+  InterpolateColors,
+  PinchGestureHandlerComponent,
+  DoubleTapAnimate,
+  ColorPicker,
+} from './src/pages';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ColorPicker />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
